e58f81739cdbc0fa2af8cecb77afdb7380e05d27
Berkay <b9bozkurt959@gmail.com>
2021-03-14 17:10:56 +0300

    Add project files.

---
+++ DictonaryClass/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryClass
+{
+    class MyDictionary<Tkey ,Tvalue>
+    {
+
+         (Tkey,Tvalue)[] list;
+
+        public MyDictionary()
+        {
+            list=new (Tkey,Tvalue)[0];
+        }
+
+
+
+        public void Ekle(Tkey key,Tvalue value) // dictionary listemize yeni eleman ekleme actionu
+        {
+            (Tkey, Tvalue)[] templArray = list;
+
+            list = new (Tkey, Tvalue)[list.Length + 1];
+
+            for (int i = 0; i < templArray.Length; i++)
+            {
+                list[i] = templArray[i];
+            }
+
+            list[list.Length-1]=(key,value);
+        }
+
+       
+
+        public int Length // dictionary listemizin kaç elemanlı olduğu gösteren properties 
+        {
+            get { return list.Length; }
+            
+        }
+
+    }
+}
---
+++ DictonaryClass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictonaryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> sehirler = new Dictionary<int, string>();
+
+            sehirler.Add(26, "Eskişehir");
+
+            Console.WriteLine(sehirler.Count);
+
+            Console.WriteLine("*******************************************************");
+
+            MyDictionary<int, string> sehirler2 = new MyDictionary<int, string>();
+
+            sehirler2.Ekle(34, "İstanbul");
+
+
+            Console.WriteLine(sehirler2.Length);
+           
+        }
+    }
+}
